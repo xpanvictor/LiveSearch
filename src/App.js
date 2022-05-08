3adea3e8574de7913f597cea364a404a51fb3e3d
xpanvictor <xpanvictor@gmail.com>
2022-05-08 16:06:41 +0100

    Add bs5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Search from "./search";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark text-white">
       <h1>Type to live search with regex</h1>
       <Search />
     </div>
